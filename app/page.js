cd5d0a9caee9aeef05787b5b375c7df65aa2cdc6
MaxHolzmann <max@holzmann.io>
2024-07-25 15:27:00 -0500

    creates an example of conditional rendering based on checkmarks being checked

--- app/page.js
+++ app/page.js
@@ -1,9 +1,34 @@
 "use client";
 import { useReactToPrint } from "react-to-print";
 import { useState, useEffect, useRef } from "react";
+import { useForm, useWatch } from "react-hook-form";
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    control,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
+  function HomeInspection({ control }) {
+    const homeInspectionCheck = useWatch({
+      control,
+      name: "homeInspectionCheck",
+    });
+
+    if (homeInspectionCheck) {
+      return (
+        <>
+          <div>CHECKED</div>
+        </>
+      );
+    }
+  }
 
   useEffect(() => {
     if (isLoading == true) {
@@ -11,12 +36,55 @@ export default function Home() {
     }
   }, [isLoading]);
 
+  /* 
+  PARTS/OPTIONS OF THE TIMELINE:
+  Home inspection?
+  Radon Test?
+  well / water / septic?
+  Utilities (ALWAYS)
+  Loan Commitment? 
+  Appraisal DATE?
+  TENATIVE CLOSING DATE, MENTION SUBJECT TO CHANGE.
+
+  options for branding
+  
+  */
+
   return (
     <>
       <div>
         <div className="">
           <h1 className="">Real Estate Transaction Timeline Generator</h1>
         </div>
+
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="border-2 flex flex-col"
+        >
+          <input
+            className="border-2"
+            defaultValue="test"
+            {...register("example")}
+          />
+
+          {/* include validation with required or other standard HTML validation rules */}
+          <input
+            className="border-2"
+            {...register("exampleRequired", { required: true })}
+          />
+
+          <input
+            type="checkbox"
+            className="border-2"
+            {...register("homeInspectionCheck", { required: true })}
+          />
+          <HomeInspection control={control}></HomeInspection>
+
+          {/* errors will return when field validation fails  */}
+          {errors.exampleRequired && <span>This field is required</span>}
+
+          <input className="rounded-md p-2 shadow-md " type="submit" />
+        </form>
       </div>
     </>
   );
